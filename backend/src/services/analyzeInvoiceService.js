88aedacaea9cec3efc0acc6cf926408d0ef2e2c8
tegar-wahyu <tegarwahyu107@gmail.com>
2025-02-27 14:14:46 +0700

    [REFACTOR] remove unused variable

--- backend/src/services/analyzeInvoiceService.js
+++ backend/src/services/analyzeInvoiceService.js
@@ -1,13 +1,3 @@
-const https = require("https");
-const { DocumentAnalysisClient, AzureKeyCredential } = require("@azure/ai-form-recognizer");
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-const endpoint = process.env.AZURE_ENDPOINT;
-const key = process.env.AZURE_KEY;
-const modelId = process.env.AZURE_MODEL_ID;
-
 const isValidPdfUrl = (documentUrl) => {
     // TODO
 };
