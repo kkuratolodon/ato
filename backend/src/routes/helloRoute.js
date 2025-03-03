0d4e1bf051090d13c45611d4f1944641bf58238b
tegar-wahyu <tegarwahyu107@gmail.com>
2025-03-03 23:27:10 +0700

    Remove hello controller, route, and associated tests

--- backend/src/routes/helloRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { sayHello } = require('../controllers/helloController');
-
-const router = express.Router();
-
-router.get('/', sayHello);
-
-module.exports = router;
