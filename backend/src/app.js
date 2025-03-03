16c076ee155dfb71fd9f504b2462e73c6f273f5e
tegar-wahyu <tegarwahyu107@gmail.com>
2025-03-03 23:29:11 +0700

    Remove unused hello route from the application

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const helloRoute = require('./routes/helloRoute');
 const analyzeRoute = require('./routes/analyzeInvoiceRoute');
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/hello', helloRoute);
 app.use('/api/invoices', analyzeRoute);
 
 app.disable('x-powered-by');
