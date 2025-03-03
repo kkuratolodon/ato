d819d854bdb491e99126e443405544929221fa10
tegar-wahyu <tegarwahyu107@gmail.com>
2025-03-03 08:27:33 +0700

    [REFACTOR] remove redundant condition

--- backend/src/controllers/analyzeInvoiceController.js
+++ backend/src/controllers/analyzeInvoiceController.js
@@ -11,9 +11,7 @@ exports.analyzeInvoice = async (req, res) => {
         const result = await analyzeInvoiceService.analyzeInvoice(documentUrl);
         res.status(200).json(result);
     } catch (error) {
-        if (error.message === "documentUrl is required") {
-            res.status(400).json({ message: error.message });
-        } else if (error.message === "Invalid PDF URL") {
+        if (error.message === "Invalid PDF URL") {
             res.status(400).json({ message: error.message });
         } else {
             res.status(500).json({ message: "Internal Server Error" });
