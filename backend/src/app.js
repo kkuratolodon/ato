ea635f8b80071b8d07e843b95a8ccd665b00ca97
suryata <madeputra415@gmail.com>
2025-03-03 08:55:58 +0700

    [REFACTOR] remove unused helloRoute from app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const helloRoute = require('./routes/helloRoute');
 const invoiceRoutes = require('./routes/invoiceRoutes')
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/hello', helloRoute);
 app.use('/api/invoices',invoiceRoutes);
 
 module.exports = app;
