338b761d1ab36f88672780e53874e122012cccd7
suryata <madeputra415@gmail.com>
2025-03-03 23:10:57 +0700

    [CHORE] Fixing Hotspot Issue - add file size limit to multer upload middleware in invoiceController

--- backend/src/controllers/invoiceController.js
+++ backend/src/controllers/invoiceController.js
@@ -3,6 +3,9 @@ const multer = require('multer');
 
 const upload = multer({
   storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 20 * 1024 * 1024, 
+  },
 });
 
 exports.uploadMiddleware = upload.single('file');
