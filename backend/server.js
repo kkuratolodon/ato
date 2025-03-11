deba4a2345ee7b935ced4773b9b1b1c5d9caf8ba
Tegar Wahyu <tegarwahyu107@gmail.com>
2025-03-11 14:01:48 +0700

    [REFACTOR] update sentry error catchinig

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,16 @@ require('dotenv').config();
 // The error handler must be registered before any other error middleware and after all controllers
 Sentry.setupExpressErrorHandler(app);
 
-// Optional fallthrough error handler
 app.use(function onError(err, req, res, next) {
+    console.error(`Error: ${err.message}`);
+    console.error(`Request URL: ${req.originalUrl}`);
+    console.error(`Request Method: ${req.method}`);
+    console.error(`Request Headers:`, req.headers);
+
     res.statusCode = 500;
     res.end((res.sentry || "Internal Server Error") + "\n");
+
+    next(err); // Forward to the next error handler if any
 });
 
 const PORT = process.env.PORT || 3000;
