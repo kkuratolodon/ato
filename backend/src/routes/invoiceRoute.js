5c9de7f3a404ee1447a56ec2d3293c44afb5c0f9
Tegar Wahyu <tegarwahyu107@gmail.com>
2025-03-24 22:53:28 +0700

    [GREEN] Implement rate limiting middleware and apply it to invoice routes

--- backend/src/routes/invoiceRoute.js
+++ backend/src/routes/invoiceRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const InvoiceController = require('../controllers/invoiceController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const apiLimiter = require('../middlewares/rateLimitMiddleware');
 
 router.get('/debug-sentry', () => {
     throw new Error("Sentry error dummy!");
@@ -9,12 +10,18 @@ router.get('/debug-sentry', () => {
 
 router.post(
     '/upload',
+    apiLimiter,
     authMiddleware,               
     InvoiceController.uploadMiddleware,
     InvoiceController.uploadInvoice
 );
 
-router.get('/:id',authMiddleware, InvoiceController.getInvoiceById);
+router.get(
+    '/:id', 
+    apiLimiter,
+    authMiddleware,
+    InvoiceController.getInvoiceById
+);
 
 router.post('/analyze', InvoiceController.analyzeInvoice);
 
