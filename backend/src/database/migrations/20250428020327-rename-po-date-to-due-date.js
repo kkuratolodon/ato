decc5c60900a36b5cda20f9b0669a5d21322b98d
Tegar Wahyu <tegarwahyu107@gmail.com>
2025-04-28 09:25:02 +0700

    refactor: remove unused Sequelize parameter from migration functions

--- backend/src/database/migrations/20250428020327-rename-po-date-to-due-date.js
+++ backend/src/database/migrations/20250428020327-rename-po-date-to-due-date.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
+  up: async (queryInterface) => {
     // Rename po_date column to due_date in PurchaseOrder table
     await queryInterface.sequelize.query(
       'ALTER TABLE dev_invoicepoocr.purchaseorder RENAME COLUMN po_date TO due_date;'
     );
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
     // Revert by renaming due_date back to po_date
     await queryInterface.sequelize.query(
       'ALTER TABLE dev_invoicepoocr.purchaseorder RENAME COLUMN due_date TO po_date;'
