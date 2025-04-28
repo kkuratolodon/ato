5fde242e669acb318bd3385be9b2ce02b5ed2c44
kkuratolodon <mif05123@gmail.com>
2025-04-28 11:09:37 +0700

    [FIX] fix migration file for rename po

--- backend/src/database/migrations/20250428020327-rename-po-date-to-due-date.js
+++ backend/src/database/migrations/20250428020327-rename-po-date-to-due-date.js
@@ -1,17 +1,21 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface) => {
+  up: async (queryInterface, Sequelize) => {
     // Rename po_date column to due_date in PurchaseOrder table
-    await queryInterface.sequelize.query(
-      'ALTER TABLE dev_invoicepoocr.purchaseorder RENAME COLUMN po_date TO due_date;'
+    await queryInterface.renameColumn(
+      'purchaseorder',  // table name
+      'po_date',       // old column name
+      'due_date'       // new column name
     );
   },
 
-  down: async (queryInterface) => {
+  down: async (queryInterface, Sequelize) => {
     // Revert by renaming due_date back to po_date
-    await queryInterface.sequelize.query(
-      'ALTER TABLE dev_invoicepoocr.purchaseorder RENAME COLUMN due_date TO po_date;'
+    await queryInterface.renameColumn(
+      'purchaseorder',  // table name
+      'due_date',      // old column name
+      'po_date'        // new column name
     );
   }
 };
